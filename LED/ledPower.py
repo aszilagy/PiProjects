2286ca4b8f301eb100b79631b73a48658552b3dc
aszilagy <aszilagy@hawk.iit.edu>
2020-09-01 18:55:50 -0500

    LED init

---
+++ LED/ledPower.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+class LED:
+    def __init__(self, channel):
+        self.GPIO_num = channel
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(channel,GPIO.OUT)
+
+    def on(self):
+        print("LED ON")
+        GPIO.output(self.GPIO_num, GPIO.HIGH)
+
+    def off(self):
+        print("LED OFF")
+        GPIO.output(self.GPIO_num, GPIO.LOW)
+
+    def on_off(self, timeToSleep):
+        self.on()
+        time.sleep(timeToSleep)
+        self.off()
+
+def main():
+    red = LED(18)
+    red.on_off(3)
+
+    blue = LED(11)
+    blue.on_off(3)
+
+if __name__ == '__main__':
+    main()
